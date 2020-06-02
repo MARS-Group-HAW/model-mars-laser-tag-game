d2d863c1b9c799f27808314ba1a57a4fffb1378e
acn934 <daniel.osterholz@haw-hamburg.de>
2020-06-02 17:38:36 +0200

    fixed csv_creator bugs

--- Analysis/csv_creator.py
+++ Analysis/csv_creator.py
@@ -13,21 +13,23 @@ def load_csv():
     # export to csv
     map_csv.to_csv("map.csv", sep=';', index=False, encoding='utf-8-sig', header=False)
 
+    li = []
     combined_csv = pd
     for f in all_filenames:
         try:
             # combine all files in the list
-            combined_csv = pd.read_csv(f.strip(), delimiter=';')
-
+            df = pd.read_csv(f.strip(), index_col=None, delimiter=';', header=0)
+            li.append(df)
         except pd.errors.EmptyDataError:
             print('Note: {} was empty. Skipping.'.format(f.replace(path, '')))
             continue  # will skip the rest of the block and move to next file
 
+    # concat file content
+    frame = pd.concat(li, axis=0, ignore_index=True)
     # sort the list
-    combined_csv.sort_values(by=['Tick'], inplace=True)
+    frame.sort_values(by=['Tick', 'color'], inplace=True)
     # export to csv
-    combined_csv.to_csv("agents.csv", sep=';', index=False, encoding='utf-8-sig')
-    # print(combined_csv)
+    frame.to_csv("agents.csv", sep=';', index=False, encoding='utf-8-sig')
 
 if __name__ == '__main__':
     load_csv()
