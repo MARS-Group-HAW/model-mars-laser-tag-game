3941071173c4c5c585f6444c95f223ae2efe8aab
Nima Ahmady <nima.ahmady-moghaddam@haw-hamburg.de>
2023-05-16 14:56:37 +0200

    docs: add details to return value description of XML doc tags
    Specify under which circumstances a method returns true, false, or null to improve understandability of method return values of the PlayerBody interface

--- LaserTagBox/Model/Body/PlayerBody.cs
+++ LaserTagBox/Model/Body/PlayerBody.cs
@@ -88,19 +88,19 @@ public class PlayerBody : MovingAgent, IPlayerBody
     /// <summary>
     ///     Explores the hills in the agent's field of vision.
     /// </summary>
-    /// <returns>A list of Hill objects</returns>
+    /// <returns>A list of Hill objects or null if the caller does not have enough ActionPoints</returns>
     public List<Position> ExploreHills1() => ExploreSpots(typeof(Hill));
 
     /// <summary>
     ///     Explores the barriers in the agent's field of vision.
     /// </summary>
-    /// <returns>A list of Barrier objects</returns>
+    /// <returns>A list of Barrier objects or null if the caller does not have enough ActionPoints</returns>
     public List<Position> ExploreBarriers1() => ExploreSpots(typeof(Barrier));
 
     /// <summary>
     ///     Explores the ditches in the agent's field of vision.
     /// </summary>
-    /// <returns>A list of Ditch objects</returns>
+    /// <returns>A list of Ditch objects or null if the caller does not have enough ActionPoints</returns>
     public List<Position> ExploreDitches1() => ExploreSpots(typeof(Ditch));
         
     /// <summary>
@@ -118,7 +118,7 @@ public class PlayerBody : MovingAgent, IPlayerBody
     /// <summary>
     ///     Explores enemy agents in the agent's field of vision.
     /// </summary>
-    /// <returns>A list of EnemySnapshot objects</returns>
+    /// <returns>A list of EnemySnapshot objects or null if the caller does not have enough ActionPoints</returns>
     public List<EnemySnapshot> ExploreEnemies1()
     {
         if (ActionPoints < 1) return null;
@@ -134,7 +134,8 @@ public class PlayerBody : MovingAgent, IPlayerBody
     ///     Determines whether there exists a direct line of sight between the caller and the given position
     /// </summary>
     /// <param name="other">The position whose line of sight relative to the caller is to be determined</param>
-    /// <returns>boolean</returns>
+    /// <returns>true if there exists an open line of sight between the caller's position and the given position.
+    /// false if no such line of sight exists or if the caller does not have enough ActionPoints.</returns>
     public bool HasBeeline1(Position other)
     {
         if (ActionPoints < 1) return false;
@@ -160,7 +161,7 @@ public class PlayerBody : MovingAgent, IPlayerBody
     ///     Shoots at the given Position. Returns true if shot was successfully executed, otherwise false.
     /// </summary>
     /// <param name="aimedPosition">The position to be shot at</param>
-    /// <returns>boolean</returns>
+    /// <returns>true if an enemy on the given position was tagged, false otherwise</returns>
     /// <exception cref="ArgumentOutOfRangeException">Thrown if a player has an invalid Stance</exception>
     public bool Tag5(Position aimedPosition)
     {
@@ -231,7 +232,7 @@ public class PlayerBody : MovingAgent, IPlayerBody
     ///     Generic exploration method for exploring points of interest (POIs) in the environment.
     /// </summary>
     /// <param name="type">The type of POI to be explored</param>
-    /// <returns>A list of POIs of the requested type</returns>
+    /// <returns>A list of POIs of the requested type or null if the caller does not have enough ActionPoints</returns>
     private List<Position> ExploreSpots(Type type)
     {
         if (ActionPoints < 1) return null;
